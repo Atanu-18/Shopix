2f6ae2c4fafbafe21059ac71cf4b751693d4a7de
atanu mandal <atanu2697@gmail.com>
2026-09-24 22:02:23 +0530

    create order done

---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,24 @@
+const Razorpay = require('razorpay');
+const crypto = require('crypto');
+dotenv = require('dotenv').config();
+
+const createdOrder = async (req, res) => {
+    try {
+        const instance = new Razorpay({
+            key_id: process.env.RAZORPAY_KEY_ID,
+            key_secret: process.env.RAZORPAY_KEY_SECRET,
+        });
+
+        const options = {
+            amount: req.body.amount * 100, // Amount in paise
+            currency: "INR",
+            receipt: crypto.randomBytes(10).toString("hex"), // Generate a random receipt ID
+        }
+        const order = await instance.orders.create(options);
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+module.exports = { createdOrder };
--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/payment', require('./routes/paymentRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> {
